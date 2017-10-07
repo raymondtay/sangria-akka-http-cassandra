f0e443ea7319cc9b00794ab90aebf97b1d71f3c6
Raymond Tay <raymond.tay@nugit.co>
2017-10-07 11:29:03 +0800

    Demonstrating the GQL query for Cassandra via Akka Http

--- src/main/scala/Data.scala
+++ src/main/scala/Data.scala
@@ -23,6 +23,27 @@ case class Droid(
   appearsIn: List[Episode.Value],
   primaryFunction: Option[String]) extends Character
 
+case class Picture(width: Int, height: Int, url: Option[String]) 
+
+trait Identifiable {
+  def id : String
+}
+case class Product(id: String, name: String, description: String) extends Identifiable {
+  def picture(size: Int): Picture = 
+    Picture(width = size, height = size, url =  Some(s"//cdn.com/$size/$id.jpg"))
+}
+
+class ProductRepo {
+  private val Products = List(
+    Product("1", "Cheesecake", "Tasty"),
+    Product("2", "Health Potion", "+50 HP"))
+
+  def product(id: String): Option[Product] =
+    Products find (_.id == id)
+
+  def products: List[Product] = Products
+}
+
 class CharacterRepo {
   import CharacterRepo._
 
